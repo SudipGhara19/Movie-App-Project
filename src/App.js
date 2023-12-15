e1f64b06054523ce89468405638aa4b64a223643
SudipGhara19 <sudipghara135@gmail.com>
2023-12-15 17:00:36 +0530

    created Navbar component and render it in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import MovieList from "./MovieList";
+import Navbar from "./Navbar";
 function App() {
   return (
     <>
-      <h1>Movie App</h1>
+      <Navbar/>
       <MovieList/>
     </>
   );
