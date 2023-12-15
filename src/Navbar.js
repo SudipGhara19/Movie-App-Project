86726f675165b026e0d50d3f447cb5226f083bf5
SudipGhara19 <sudipghara135@gmail.com>
2023-12-15 23:49:26 +0530

    added CSS Scope in react. created a object styles and and put the styles in it

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,21 +3,56 @@ import React from "react";
 class Navbar extends React.Component{
     render(){
         return(
-            <>
-            <div style={{width: "100%", 
-                        height: 90, 
-                        backgroundColor: "blue", 
-                        display: "flex", 
-                        justifyContent: "space-between"}}>
-                <div>Movie-App</div>
-                <div>
-                    <img alt="cart icon"/>
-                    <span>0</span>
-                </div>
-            </div>
-            </>
+            <div style={styles.nav}>
+        <div style = {styles.title}>Movie-App</div>
+        <div style={styles.cartIconContainer}>
+          <img
+            style={styles.cartIcon}
+            src="https://cdn-icons-png.flaticon.com/128/1170/1170576.png"
+            alt="cart-icon"
+          />
+          <span style={styles.cartCount}>3</span>
+        </div>
+      </div>
         )
     }
 }
+
+const styles = {
+    cartIcon: {
+      height: 48,
+      marginRight: 20,
+    },
+    nav: {
+      height: 70,
+      background: "#4267b2",
+      display: "flex",
+      justifyContent: "space-between" ,
+      alignItems: "center",
+      position: "relative",
+    },
+    title:{
+      fontSize: 30,
+      color: "#fff",
+      fontWeight: 600,
+      fontFamily: '"Montserrat", sans-serif',
+      textTransform: "uppercase",
+      marginLeft: 20
+    },
+    
+    cartIconContainer: {
+      position: "relative",
+      cursor: "pointer",
+    },
+    cartCount: {
+      background: "orange",
+      borderRadius: "50%",
+      padding: "4px 8px",
+      position: "absolute",
+      right: 10,
+      top: -5,
+      fontSize: 12,
+    },
+  };
 
 export default Navbar;
